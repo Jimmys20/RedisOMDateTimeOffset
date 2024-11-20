54eae30824f571a02bc80144e05c1f04e52b2a1e
jimmys20 <maragkosd12@gmail.com>
2024-11-21 00:48:03 +0200

    Add project files.

---
+++ RedisOMDateTimeOffset/PlayerEntity.cs
@@ -0,0 +1,10 @@
+﻿using Redis.OM.Modeling;
+
+namespace RedisOMDateTimeOffset;
+
+[Document(StorageType = StorageType.Json, Prefixes = ["Player"])]
+public class PlayerEntity
+{
+    [RedisIdField, Indexed] public string UniquePlayerId { get; set; }
+    public DateTimeOffset StartTime { get; set; }
+}
---
+++ RedisOMDateTimeOffset/Program.cs
@@ -0,0 +1,44 @@
+﻿using Redis.OM;
+
+namespace RedisOMDateTimeOffset;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var redisConfigurationOptions = new StackExchange.Redis.ConfigurationOptions
+        {
+            EndPoints = { "192.168.100.4:6379" },
+            User = null,
+            Password = null,
+            AllowAdmin = false,
+            Ssl = false,
+        };
+
+        var provider = new RedisConnectionProvider(redisConfigurationOptions);
+
+        provider.Connection.DropIndexAndAssociatedRecords(typeof(PlayerEntity));
+        await provider.Connection.CreateIndexAsync(typeof(PlayerEntity));
+
+        var players = provider.RedisCollection<PlayerEntity>();
+
+        var uniquePlayerId = Guid.NewGuid().ToString();
+
+        await players.InsertAsync(new PlayerEntity
+        {
+            UniquePlayerId = uniquePlayerId,
+            StartTime = new DateTimeOffset(2024, 11, 21, 12, 30, 00, new TimeSpan(1, 0, 0)),
+        });
+
+        var player = await players.FindByIdAsync(uniquePlayerId);
+
+        player.StartTime = new DateTimeOffset(2024, 11, 21, 12, 45, 00, new TimeSpan(1, 0, 0));
+
+        await players.SaveAsync();
+
+        var player2 = await players.FindByIdAsync(uniquePlayerId);
+
+        Console.WriteLine(player2.StartTime);
+        Console.WriteLine(player.StartTime.Offset == player2.StartTime.Offset);
+    }
+}
